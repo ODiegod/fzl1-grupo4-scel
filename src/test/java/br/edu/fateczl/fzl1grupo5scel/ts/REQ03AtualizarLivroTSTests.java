7d9dda7fc69946b63cad0af512ad9d1b541ed12e
ODiegod <57514473+ODiegod@users.noreply.github.com>
2021-06-03 18:32:27 -0300

    Add files via upload
    projeto

---
+++ src/test/java/br/edu/fateczl/fzl1grupo5scel/ts/REQ03AtualizarLivroTSTests.java
@@ -0,0 +1,43 @@
+package br.edu.fateczl.fzl1grupo5scel.ts;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import br.edu.fateczl.fzl1grupo5scel.po.LivroPageObject;
+import br.edu.fateczl.fzl1grupo5scel.po.LoginPageObject;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class REQ03AtualizarLivroTSTests {
+
+    private WebDriver driver;
+    private LivroPageObject livroPO;
+    private LoginPageObject loginPO;
+
+    @BeforeEach
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "browserDriver/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://ts-scel.herokuapp.com");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    void CT01_LivroAlteradoComSucesso() {
+        loginPO = new LoginPageObject(driver);
+        livroPO = new LivroPageObject(driver);
+        loginPO.login("jose", "123");
+        livroPO.cadastrar("1571", "Carlos Alberto Heuser", "Projeto de Banco de Dados");
+        livroPO.atualizar("", "Carlos A. Heuser", "Banco de Dados");
+        assertEquals("Carlos A. Heuser", livroPO.getResult());
+        livroPO.excluir();
+    }
+}
---
+++ src/test/java/br/edu/fateczl/fzl1grupo5scel/ts/REQ04ExcluirLivroTSTests.java
@@ -0,0 +1,51 @@
+package br.edu.fateczl.fzl1grupo5scel.ts;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import br.edu.fateczl.fzl1grupo5scel.po.LivroPageObject;
+import br.edu.fateczl.fzl1grupo5scel.po.LoginPageObject;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class REQ04ExcluirLivroTSTests {
+
+    private WebDriver driver;
+    private LivroPageObject livroPO;
+    private LoginPageObject loginPO;
+
+    @BeforeEach
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "browserDriver/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://ts-scel.herokuapp.com");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+
+    public void esperar() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void CT01_LivroExcluidoComSucesso() {
+        loginPO = new LoginPageObject(driver);
+        livroPO = new LivroPageObject(driver);
+        loginPO.login("jose", "123");
+        livroPO.cadastrar("1571", "Carlos Alberto Heuser", "Projeto de Banco de Dados");
+        livroPO.excluir();
+        assertThrows(NoSuchElementException.class, () -> { livroPO.getResult(); });
+    }
+}
